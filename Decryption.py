013d239da63a8b7ead1c339b2ba543d4499805e8
Adrian Robert Doroteo <adrian56th@gmail.com>
2022-05-15 13:49:54 +0800

    removed binary to string conversion of cipherText since mongodb supports binary data

--- Decryption.py
+++ Decryption.py
@@ -7,8 +7,8 @@ with open('cipher_file', 'rb') as c_file:
     iv = c_file.read(16)
     cipherText = c_file.read()
 
-cipher = AES.new(key,AES.MODE_CBC, iv)
+cipher = AES.new(key, AES.MODE_CBC, iv)
 
-plainText = unpad(cipher.decrypt(cipherText),AES.block_size)
+plainText = unpad(cipher.decrypt(cipherText), AES.block_size)
 
 print(plainText)
--- Encryption.py
+++ Encryption.py
@@ -6,21 +6,27 @@ import pymongo
 from pymongo import MongoClient
 import datetime
 from datetime import datetime
+import random
 
-cluster = MongoClient('mongodb+srv://test1:123@cluster0.hfj9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+cluster = MongoClient(
+    'mongodb+srv://test1:123@cluster0.hfj9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 db = cluster['Flame']
 collection = db['arjdoroteo']
 
 # Encrypts user's name and location, writes to file, and returns cipher
+
+
 def aes_userdata():
-    key = get_random_bytes(32)
-    iv = get_random_bytes(16)
+    # key = get_random_bytes(32)
+    # iv = get_random_bytes(16)
+    key = b'1234567890qwertyuiopasdfghjklzxc'
+    iv = b'1q2w3e4r5t6y7u8i'
     # plainName = input('Enter Name: ').encode('ascii')
     # plaintLocation = input('Enter Location: ').encode('ascii')
     plainName = b'Adrian Robert Doroteo'
     plainLocation = b'Las Pinas City'
-    plainData = plainName +b','+ plainLocation
-    cipher = AES.new(key,AES.MODE_CBC,iv)
+    plainData = plainName + b',' + plainLocation
+    cipher = AES.new(key, AES.MODE_CBC, iv)
 
     cipherText = cipher.encrypt(pad(plainData, AES.block_size))
 
@@ -28,22 +34,22 @@ def aes_userdata():
         c_file.write(key)
         c_file.write(iv)
         c_file.write(cipherText)
-    return str(cipherText)
+    return (cipherText)
 
-#Uploads data to Mongodb
+# Uploads data to Mongodb
+
+
 def mongodbUpload(temp, co, gas, date_time, cipherText):
-    post = {'User Info':cipherText,'Date and Time': date_time,'Temperature' : temp, 'CO': co, 'LPG': gas}
+    post = {'User Info': cipherText, 'Date and Time': date_time,
+            'Temperature': temp, 'CO': co, 'LPG': gas}
     collection.insert_one(post)
-    print ('Uploaded')
+    print('Uploaded')
+
 
 x = True
-temp = 69
+
 temp_limit = 125
-
-co = 50
 co_limit = 100
-
-gas = 150
 gas_limit = 10000
 
 timer = 5
@@ -51,7 +57,10 @@ timer = 5
 cipherText = aes_userdata()
 
 while x == True:
-    
+    temp = random.randint(15, 69)
+    co = random.randint(0, 100)
+    gas = random.randint(0, 10000)
+
     now = datetime.now()
     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 
@@ -66,6 +75,7 @@ while x == True:
         print('Timer Done!')
         timer = 5
 
-    timer-= 1
+    timer -= 1
     time.sleep(1)
     print(timer)
+    print('Temp: ' + str(temp) + ' CO: ' + str(co) + ' LPG: ' + str(gas))

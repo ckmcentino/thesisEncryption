f7559cab777d77f89fc5d7087abc5a28ee42fb48
ckmcentino <61411871+ckmcentino@users.noreply.github.com>
2022-06-03 15:22:10 +0800

    Update Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -1,100 +1,71 @@
-# install the following libraries
-# pip install pycryptodome
-# pip install pymongo
-# pip install "pymongo[srv]"
-
-# import the needed libraries
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from Crypto.Random import get_random_bytes
 import time
-import pymongo
-from pymongo import MongoClient
+import pumongo
+from pumong import MongoClient
 import datetime
 from datetime import datetime
 import random
 
-#Establishing the connection between the database and the program
 cluster = MongoClient(
     'mongodb+srv://test1:123@cluster0.hfj9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 db = cluster['Flame']
 collection = db['arjdoroteo']
 
-
-# Encrypts user's name and location, writes to file, and returns cipher
-
 def aes_userdata():
-    # For final testing, random keys and initialization vector would be generated and used.
-    # key = get_random_bytes(32)
-    # iv = get_random_bytes(16)
-
-    # for testing purposes, predefined keys and initialization vector are used.
     key = b'1234567890qwertyuiopasdfghjklzxc'
     iv = b'1q2w3e4r5t6y7u8i'
-
-    # User input of Name and Location
-    # plainName = input('Enter Name: ').encode('ascii')
-    # plaintLocation = input('Enter Location: ').encode('ascii')
-
-    # predefined Name and Location
+    
     plainName = b'Adrian Robert Doroteo'
     plainLocation = b'14.466762,120.974886'
     plainData = plainName + b',' + plainLocation
-
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-
-    cipherText = cipher.encrypt(pad(plainData, AES.block_size))
-
-    # Writes the key, iv, and encrypted text to a local file
-    with open('cipher_file', 'wb') as c_file:
+    
+    cipher = AES.new(key,AES.MODE_CBC,iv)
+    
+    cipherText = cipher.encrypt(pad(plainData,AES.block_size))
+    
+    with open('cipher_file','wb') as c_file:
         c_file.write(key)
         c_file.write(iv)
         c_file.write(cipherText)
-
-    # returns the encrypted text
+        
     return (cipherText)
 
-
-# Function to upload data to mongodb
 def mongodbUpload(temp, co, gas, date_time, cipherText):
-    post = {'User Info': cipherText, 'Date and Time': date_time,
-            'Temperature': temp, 'CO': co, 'LPG': gas}
+    post = {'User Info': cipherText, 'Date and Time': date_time, 'Temperature': temp, 'CO': co, 'LPG': gas}
     collection.insert_one(post)
     print('Uploaded')
-
-
+    
 x = True
 
 temp_limit = 125
 co_limit = 100
 gas_limit = 10000
 
-# timer set to 5 seconds, will be replaced to 1 hour in final system
 timer = 5
 
 cipherText = aes_userdata()
 
 while x == True:
-    #random data will be replaced by data from sensors
-    temp = random.randint(15, 125)
-    co = random.randint(0, 100)
-    gas = random.randint(0, 10000)
-
+    temp = random.randint(15,125)
+    co = random.randint(0,100)
+    gas = random.randint(0,10000)
+    
     now = datetime.now()
-    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S") 
     if temp >= temp_limit or co >= co_limit or gas >= gas_limit:
         mongodbUpload(temp, co, gas, date_time, cipherText)
         if timer == 0:
             print('Timer Done!')
             timer = 5
-
-    elif timer == 0:
+            
+    elif time == 0:
         mongodbUpload(temp, co, gas, date_time, cipherText)
         print('Timer Done!')
         timer = 5
-
+        
     timer -= 1
     time.sleep(1)
     print(timer)
-    print('Temp: ' + str(temp) + ' CO: ' + str(co) + ' LPG: ' + str(gas))
+    print('Temp: ' + str(temp) + ' CO: ' + str(C0) + ' LPG: ' + str(gas)) 
